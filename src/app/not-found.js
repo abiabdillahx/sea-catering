6234d7f1523cc10e715e5699619b15675eecb33f
Abi <83850574+abiabdillahx@users.noreply.github.com>
2025-06-22 18:35:16 +0700

    feat: modify customized 404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,14 +1,14 @@
 import Link from "next/link";
-import { ChefHat } from "lucide-react";
+import { CloudAlert } from "lucide-react";
 import Navbar from "@/components/Navbar";
 
 export default function NotFound(){
     return (
         <>
-            <div className="grid grid-rows-[10px_1fr_10px] items-center justify-items-center mt-50 font-outfit">
+            <div className="grid grid-rows-[10px_1fr_10px] items-center justify-items-center my-50 font-outfit bg-background">
                 <main className="flex flex-col gap-[20px] row-start-2 justify-items-center items-center">
                     <div className="flex flex-row items-center gap-3 text-7xl">
-                        <ChefHat className="w-10 h-10 sm:w-20 sm:h-20"></ChefHat>
+                        <CloudAlert className="w-10 h-10 sm:w-20 sm:h-20"></CloudAlert>
                         <h1 className="font-bold item-center text-7xl">Not Found</h1>
                     </div>
                     <p className="text-xl">The page you're looking for isn't here vro...</p>
@@ -20,10 +20,10 @@ export default function NotFound(){
                             Back to Home
                         </Link>
                         <Link
-                            className='text-foreground px-6 py-4 rounded-full duration-200 bg-background border-primary hover:bg-foreground '
-                            href='/'
+                            className='text-foreground px-6 py-4 rounded-full duration-200 bg-background border-2 border-primary hover:bg-secondary hover:text-background hover:border-transparent'
+                            href='/login'
                         >
-                            Back to Home
+                            Log In
                         </Link>
                     </div>
 
